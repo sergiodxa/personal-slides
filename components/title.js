f18b5bfbe792939d6dfcab491cc791b175d09ba2
Sergio Xalambrí <sergiodxa@gmail.com>
2018-04-14 23:21:40 -0500

    Add support for multiple headings

--- components/title.js
+++ components/title.js
@@ -1,4 +1,4 @@
-export default ({ children }) => (
+export const H1 = ({ children }) => (
   <h1>
     {children}
     <style jsx>{`
@@ -10,4 +10,32 @@ export default ({ children }) => (
       }
     `}</style>
   </h1>
+);
+
+export const H2 = ({ children }) => (
+  <h2>
+    {children}
+    <style jsx>{`
+      h2 {
+        font-weight: lighter;
+      }
+      h2 :global(strong) {
+        font-weight: normal;
+      }
+    `}</style>
+  </h2>
+);
+
+export const H3 = ({ children }) => (
+  <h3>
+    {children}
+    <style jsx>{`
+      h3 {
+        font-weight: lighter;
+      }
+      h3 :global(strong) {
+        font-weight: normal;
+      }
+    `}</style>
+  </h3>
 );
