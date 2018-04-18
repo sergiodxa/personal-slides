5a5d2d84d041ed4759560c50cd2f1a08d8ff8f3f
Sergio Xalambrí <sergiodxa@gmail.com>
2018-04-18 18:07:11 -0500

    Terminal caret

---
+++ components/terminal/caret.js
@@ -0,0 +1,15 @@
+import { white } from "../../lib/colors"
+
+export default () => (
+  <span>
+    <style jsx>{`
+      span {
+        background: ${white};
+        display: inline-block;
+        width: 7px;
+        height: 15px;
+        vertical-align: middle;
+      }
+    `}</style>
+  </span>
+);
