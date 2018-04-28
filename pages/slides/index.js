427f433c2624b1ed3f4ba6e14e00884e417931c0
Sergio Xalambrí <sergiodxa@gmail.com>
2018-04-28 08:25:38 -0500

    Add list of slides

--- pages/slides/index.js
+++ pages/slides/index.js
@@ -1,3 +1,30 @@
-import redirect from '../../lib/redirect'
+import Slide from "components/slide";
+import { H1 } from "components/title";
+import { List, Item } from "components/list";
+import Link from "components/link";
 
-export default redirect('/slides/serverless')
+export default () => (
+  <Slide
+    basePath="/slides"
+    current={1}
+    total={1}
+    title="Sergio Xalambrí's Slides"
+    center
+    dark
+  >
+    <H1>Slides</H1>
+    <List>
+      <Item>
+        <Link href="/slides/serverless/1">
+          Introduction to <strong>Serverless</strong>
+        </Link>
+      </Item>
+      <Item>
+        <Link href="/slides/local-to-global/main">
+          From <strong>Local</strong> to <strong>Global</strong> with a Single
+          Command
+        </Link>
+      </Item>
+    </List>
+  </Slide>
+);
