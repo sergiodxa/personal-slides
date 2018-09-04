1b29eaeeba8fe65564bb2ccb4388c4450ec4e168
Sergio Xalambrí <sergiodxa@gmail.com>
2018-09-04 16:56:47 -0500

    Add code example to slide

--- pages/slides/micro-frontends/next/use-case-bonus.js
+++ pages/slides/micro-frontends/next/use-case-bonus.js
@@ -1,5 +1,16 @@
 import Slide from "components/slide";
 import { H1 } from "components/title";
+import Code from "components/code";
+
+const code = `const { NOW_URL } = process.env;
+const { alias } = require("./now.json");
+
+module.exports = {
+  assetPrefix: NOW_URL
+    ? \`https://\${alias[1]}\`
+    : "http://localhost:3000"
+};
+`
 
 export default () => (
   <Slide
@@ -11,5 +22,6 @@ export default () => (
     dark
   >
     <H1>This Slides</H1>
+    <Code>{code}</Code>
   </Slide>
 );
